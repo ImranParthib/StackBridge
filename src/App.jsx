9a1effb47c912dfa46765e720c8ed7dc2889c2ec
ImranParthib <imranparthib13@gmail.com>
2025-01-06 00:32:44 +0600

    ✨ reorganize imports in App component for improved structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
- 
-import { Route, Routes } from "react-router-dom";
 import AnimatedCursor from "react-animated-cursor";
+import { Route, Routes } from "react-router-dom";
 
+import "./App.css";
 import AboutUs from "./components/AboutUs";
-import OurServices from "./components/OurServices";
-import OurProjects from "./components/OurProjects";
-import OurProducts from "./components/OurProducts";
+import BackToTopButton from "./components/BackToTopButton";
 import Blogs from "./components/Blogs";
 import ContactUs from "./components/ContactUs";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./App.css";
+import Header from "./components/Header";
 import Home from "./components/Home/Home";
-import BackToTopButton from "./components/BackToTopButton";
+import OurProducts from "./components/OurProducts";
+import OurProjects from "./components/OurProjects";
+import OurServices from "./components/OurServices";
 
 function App() {
   return (
